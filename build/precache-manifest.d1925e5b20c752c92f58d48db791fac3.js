9cea39eebfbba15db8d937cc7ac15028fa27f0dd
Admire Khulumo <admirekhulumo@Admire-MBA.local>
2020-06-16 13:08:50 +0200

    build folder added

---
+++ build/precache-manifest.d1925e5b20c752c92f58d48db791fac3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44ac07496789a3d47d6147452bfd7e56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e12859f16a7c3b4d1a03",
+    "url": "/static/css/main.22de92b1.chunk.css"
+  },
+  {
+    "revision": "1a9a444a68637f0a0063",
+    "url": "/static/js/2.9427269d.chunk.js"
+  },
+  {
+    "revision": "fbfb5e42a41e7004782949e93fdf296a",
+    "url": "/static/js/2.9427269d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e12859f16a7c3b4d1a03",
+    "url": "/static/js/main.7e7938d3.chunk.js"
+  },
+  {
+    "revision": "14dda29475bb1924d80c",
+    "url": "/static/js/runtime-main.1fe119f2.js"
+  },
+  {
+    "revision": "3704ce0ca4c44394155110b06802b763",
+    "url": "/static/media/biust-logo.3704ce0c.png"
+  },
+  {
+    "revision": "bd5bc3ce35df6ec6b3f6cc9c76998b92",
+    "url": "/static/media/biust_logo_small.bd5bc3ce.png"
+  }
+]);
